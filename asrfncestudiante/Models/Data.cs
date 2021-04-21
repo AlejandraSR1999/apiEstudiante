2c2475e8e6ecfe7db10c8984a33916ff62310d56
Alejandra Salazar Rojas <a2017113913@estudiantes.upsa.edu.bo>
2021-04-20 20:46:34 -0400

    first commit

---
+++ asrfncestudiante/Models/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace asrfncestudiante.Models
+{
+    class Data
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Estudiante { get; set; }
+
+        [Required]
+        public string Temperatura { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDateTime { get; set; }
+    }
+}
