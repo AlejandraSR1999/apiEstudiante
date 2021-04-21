9a00c5a484dbd90dc2d44f4ce5a1cc571c026555
Alejandra Salazar Rojas <a2017113913@estudiantes.upsa.edu.bo>
2021-04-20 20:28:43 -0400

    Add project files.

---
+++ apiEstudiante/Controllers/DataController.cs
@@ -0,0 +1,43 @@
+﻿using apiEstudiante.Models;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiEstudiante.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+
+            string connectionString = "Endpoint=sb://asrservicebusexamen.servicebus.windows.net/;SharedAccessKeyName=Enviar;SharedAccessKey=lojvYiX4m9RMFw5ffl0fCSQlzUw8NHAYqOZVPtAAfpk=;EntityPath=asrcolaexamen";
+            string queueName = "asrcolaexamen";
+            string mensaje = JsonConvert.SerializeObject(data);
+
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+
+            return true;
+        }
+    }
+
+}
---
+++ apiEstudiante/Models/Data.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiEstudiante.Models
+{
+    public class Data
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Estudiante { get; set; }
+
+        [Required]
+        public string Temperatura { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDateTime { get; set; }
+
+    }
+}
+
